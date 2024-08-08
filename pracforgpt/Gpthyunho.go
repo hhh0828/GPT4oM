04344889eb18e0eb030b7f2879292d3048632f24
hyunho.hong <hhhcjswo@naver.com>
2024-08-08 16:54:55 +0900

    "exclude Docker"

--- pracforgpt/Gpthyunho.go
+++ pracforgpt/Gpthyunho.go
@@ -7,9 +7,6 @@ import (
 	"fmt"
 	"net/http"
 	"os"
-
-	"github.com/labstack/echo/v4"
-	"github.com/labstack/echo/v4/middleware"
 )
 
 type Request struct {
@@ -368,24 +365,7 @@ func main() {
 	req := userreq.MakingRequest()
 	rawres := GetResponse(req)
 	//part of Docker container response.
-	e := echo.New()
 
-	e.Use(middleware.Logger())
-	e.Use(middleware.Recover())
-
-	e.GET("/", func(c echo.Context) error {
-		return c.HTML(http.StatusOK, "Hello, Docker! <3")
-	})
-
-	e.GET("/health", func(c echo.Context) error {
-		return c.JSON(http.StatusOK, struct{ Status string }{Status: "OK"})
-	})
-
-	httpPort := os.Getenv("PORT")
-	if httpPort == "" {
-		httpPort = "8080"
-	}
-	e.Logger.Fatal(e.Start(":" + httpPort))
 	//fmt.Println(rawres)
 	res := new(Response)
 	transfromedres := TransformRes(rawres, res)
